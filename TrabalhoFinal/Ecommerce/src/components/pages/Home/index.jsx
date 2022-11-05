fa63105b304e3464bb9e69b374db3f1496bf7016
Thamires Ouverney <touverneydarci@gmail.com>
2022-11-05 19:04:31 -0300

    Teste

--- TrabalhoFinal/Ecommerce/src/components/pages/Home/index.jsx
+++ TrabalhoFinal/Ecommerce/src/components/pages/Home/index.jsx
@@ -1,11 +1,26 @@
 import './style.css'
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function Home() {
     return (
-        <main>
+        <div>
             <h1>Home</h1>
+        </div>
+        // <Container>
+        //     <Row className="justify-content-md-center">
+        //         <Col xs lg="2">
+        //                     1 of 3
+        //         </Col>
+        //         <Col md="auto">Variable width content</Col>
+        //         <Col xs lg="2">
+        //                     3 of 3
+        //         </Col>
+        //     </Row>
+        // <Container />
+
         
-        </main>
     )
 }
+
 export default Home
