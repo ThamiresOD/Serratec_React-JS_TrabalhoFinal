68c878649108fcb50fd77e0e5d8c7fe271c495db
Thamires Ouverney <touverneydarci@gmail.com>
2022-11-06 22:04:27 -0300

    Alteração incompleta na Home e Produto

--- TrabalhoFinal/Ecommerce/src/components/pages/Home/index.jsx
+++ TrabalhoFinal/Ecommerce/src/components/pages/Home/index.jsx
@@ -8,10 +8,15 @@ import slide3 from '../Images/slide3.jpg'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Carousel from 'react-bootstrap/Carousel'
-import Accordion from 'react-bootstrap/Accordion';
 
 
 function Home() {
+    const items = [
+        { id: 1, title: 'Titulo 1', descricao: 'Descrição 1', categoria: 'categoria 1' },
+        { id: 2, title: 'Titulo 2', descricao: 'Descrição 2', categoria: 'categoria 2' }
+    ]
+
+
     return (
         <>
             <Header />
@@ -61,64 +66,102 @@ function Home() {
                     <h1>Menu de Categorias</h1>
                 </div>
 
-                <div className='accordion'>
 
-                    <Accordion defaultActiveKey="0" >
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header>Categoria 1</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum.
-                            </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey="1">
-                            <Accordion.Header>Categoria 2</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum.
-                                <div className='categorias'>
-                                    <Card className="categoria" style={{ width: '18rem' }}>
-                                        <Card.Img variant="top" src={fotoProduto} />
-                                        <Card.Body>
-                                            <Card.Title>Card Title: categoria 1</Card.Title>
-                                            <Card.Text>
-                                                Some quick example text to build on the card title and make up the
-                                                bulk of the card's content.
-                                            </Card.Text>
-                                            <Button variant="Dark">Go somewhere</Button>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card className="categoria" style={{ width: '18rem' }}>
-                                        <Card.Img variant="top" src={fotoProduto} />
-                                        <Card.Body>
-                                            <Card.Title>Card Title: categoria 2</Card.Title>
-                                            <Card.Text>
-                                                Some quick example text to build on the card title and make up the
-                                                bulk of the card's content.
-                                            </Card.Text>
-                                            <Button variant="Dark">Go somewhere</Button>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
+                <div className='categorias'>
+
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 1</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className='categorias'>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 1</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card className="categoria" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={fotoProduto} />
+                        <Card.Body>
+                            <Card.Title>Card Title: categoria 2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="Dark">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                 </div>
 
-
-
-
-
+                
             </main>
 
             <Footer />
