f1ab85bbad05ef886838c502017d2fed01cbc9e2
Thaisa Muniz <110869515+thaisamuniz89@users.noreply.github.com>
2022-11-08 15:56:12 -0300

    crud -produtos
    add CRUD de produtos (Andre-Toigo)

--- TrabalhoFinal/Ecommerce/src/components/Cards/index.jsx
+++ TrabalhoFinal/Ecommerce/src/components/Cards/index.jsx
@@ -1,16 +1,39 @@
 import { Card, Button } from 'react-bootstrap';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-const CardProduto = ({ produto }) => {
+const CardProduto = ({ produto, deleteProduto, setDescricao, setPreco, setImagUrl, setTitulo, setEditing }) => {
+        
+
+    const handleUpdate = () => {
+        console.log(produto)
+        setDescricao(produto.descricao)
+        setPreco(produto.preco)
+        setTitulo(produto.titulo)
+        setImagUrl(produto.imagUrl)
+        setEditing({id: produto.id, edit: true})
+    }  
+    
+      
     return (
-        <Card className="mb-2">
-            <Card.Header as="h5">{produto.titulo}</Card.Header>
-            <Card.Body>
-                <Card.Text>
-                {produto.descricao}
-                </Card.Text>
-            </Card.Body>
+      <CardGroup>
+        <Card>
+          <Card.Img variant="top" src={produto.imagUrl} />
+          <Card.Body>
+            <Card.Title>{produto.titulo}</Card.Title>
+            <Card.Text>
+            {produto.descricao}
+            </Card.Text>
+            <Button onClick={() => deleteProduto(produto.id)}>Excluir</Button>
+            <Button className="ms-3" onClick={handleUpdate}>Editar</Button>
+          </Card.Body>
+          <Card.Footer>
+          <small className="text-muted">Preço: R$ {produto.preco}</small>
+          </Card.Footer>
         </Card>
-    )
-}
-
-export default CardProduto
+        </CardGroup>
+        
+    );
+    
+  }
+  
+  export default CardProduto;
--- TrabalhoFinal/Ecommerce/src/components/pages/Admin/index.jsx
+++ TrabalhoFinal/Ecommerce/src/components/pages/Admin/index.jsx
@@ -1,31 +1,94 @@
 import { useState, useEffect } from 'react'
 import './style.css'
-import api from  '../../../service/api'; 
+import api from '../../../service/api';
 import { Button, Container, Form, Stack } from 'react-bootstrap';
-import CardProduto from '../../../components/Cards';
+import CardProduto from '../../Cards'
 
 
-function Admin() {
-  const [produto, setProduto] = useState([]);
+
+
+function App() {
+  //const [produto, setProduto] = useState([]);
   const [titulo, setTitulo] = useState("")
   const [descricao, setDescricao] = useState("")
   const [preco, setPreco] = useState("")
-   const [produtoList, setProdutoList] = useState([])
+  const [produtoList, setProdutoList] = useState([])
+  const [isEditing, setEditing] = useState({ id: "", edit: false});
+  const [imagUrl, setImagUrl] = useState("")
   
-  useEffect(() => {
-    const getProduto = async () => {
+   
+
+    const getProdutos = async () => {
       const { data } = await api.get("/produtos");
       setProdutoList(data);      
     }
-    getProduto();
+
+    useEffect(() => {
+    getProdutos()
   }, []);
   
+
+
+  const reset = () => {
+    setImagUrl("")
+    setTitulo("")
+    setDescricao("")
+    setPreco("")
+    setEditing({id: "", edit: false})
+  }
+
+
+
+  const updateProdutoList = async () => {
+    const newProduto = {
+      imagUrl: imagUrl,
+      titulo: titulo,
+      descricao: descricao,
+      preco: preco,
+    };
+
+    const { data, status } = await api.put(`/produtos/${isEditing.id}`, newProduto);
+    console.log(data, status);
+    if (status == 200) {
+      const updatedProdutoList = produtoList.map(produto => {
+        if(produto.id == data.id){
+          return data
+        }
+        return produto
+      })
+      setProdutoList(updatedProdutoList)
+    }
+    setImagUrl("");
+    setTitulo("");
+    setDescricao("");
+    setPreco("");
+    setEditing({id: "", edit: false})
+  }
+  useEffect(() => {
+    getProdutos();
+  }, []);
+
+
+  const deleteProduto = async (id) => {
+    try {
+      const { data, status } = await api.delete(`/produtos/${id}`);
+      console.log(data, status);
+      if (status == 200) {
+        const updatedProdutoList = produtoList.filter((item) => item.id != data.id);
+        setProdutoList(updatedProdutoList);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const handleProduto = async () => {
     if(titulo == "" || preco == "" || descricao == "" ) {
       alert("Preencha todos os campos")
       return
     }
 const newProduto = {
+imagUrl: imagUrl,
 titulo: titulo,
 descricao: descricao,
 preco: preco,
@@ -36,7 +99,7 @@ const {data} = await api.post("/produtos", newProduto)
 setProdutoList([...produtoList, data])
 console.log(data)
 
-
+setImagUrl("")
 setTitulo("")
 setDescricao("")
 setPreco("")
@@ -57,28 +120,53 @@ setPreco("")
         </Form.Group>
         <Form.Group className="mb-3" >
         <Form.Label>Preço:</Form.Label> 
-        <Form.Control type="text" placeholder="Insira o Preço" onChange={ e => setPreco(e.target.value)} value={preco} /> 
+        <Form.Control type="text" placeholder="Insira o preço" onChange={ e => setPreco(e.target.value)} value={preco} /> 
+        </Form.Group>
+        <Form.Group className="mb-3" >
+        <Form.Label>Imagem:</Form.Label> 
+        <Form.Control type="text" placeholder="Insira a url da imagem" onChange={ e => setImagUrl(e.target.value)} value={imagUrl} /> 
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Descrição: </Form.Label>
           <Form.Control as="textarea" rows={3} onChange={ e => setDescricao(e.target.value)} value={descricao} />
+        
         </Form.Group>
         <Stack className="mb-3">
-        <Button className="float-end" onClick={handleProduto}>Cadastrar novo produto</Button>
+          {isEditing.edit ?  (
+          <>
+          <Button className="" onClick={updateProdutoList}>
+                Salvar Produto
+              </Button>
+              <Button className="mt-2" onClick={reset}>
+                Cancelar
+              </Button>
+              </>
+           ) :
+           (<Button className="" onClick={handleProduto}>
+              Cadastrar Novo Produto
+            </Button>)
+            }
+      
         </Stack>
       </Form>
 
-      <Stack>
-            {produtoList.length > 0 && produtoList.map( item => {
-            return <CardProduto key={item.id} produto={item}/>
-          })}
-      </Stack>
+<Stack>
+      {produtoList.length > 0 && produtoList.map( item => {
+      return <CardProduto key={item.id} produto={item} deleteProduto={deleteProduto}
+      setImagUrl={setImagUrl}
+      setTitulo={setTitulo}
+      setDescricao={setDescricao}
+      setPreco={setPreco}
+      setEditing={setEditing}/>
+      
+    })} 
+</Stack>
 
 
-      </Container>
+</Container>
 
-      </>
+ </>
   )
 }
 
-export default Admin;
+export default App
