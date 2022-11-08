9ca828ea6a18865cc8ba09a4f800a35b4f4586ad
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2022-11-07 23:56:41 -0300

    Consertando os links do header

--- TrabalhoFinal/Ecommerce/src/components/Header/header.jsx
+++ TrabalhoFinal/Ecommerce/src/components/Header/header.jsx
@@ -5,8 +5,12 @@ import Logo from "../pages/Images/logo.png";
 import "./style.css";
 import { AuthContext } from "../contexts/auth";
 import { useContext } from "react";
+import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Header = () => {
+  const [loginOut, SetLoginOut] = useState("Login")
   const { logout } = useContext(AuthContext)
 
   const handleLogout = () => {
@@ -14,21 +18,28 @@ const Header = () => {
     navigate("/");
   }
 
+  // useEffect(()=> {
+  //   if(user != null){
+  //     SetLoginOut("Logout")
+  //   }
+  // },[]);
+  
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container fluid>
           <div className="logo">
             <img src={Logo} width="80px"></img>
-            <Navbar.Brand className="nomeLoja" href={"/home"}>Aquarium</Navbar.Brand>
+            <Navbar.Brand className="nomeLoja" as={Link} to={"/home"}>Aquarium</Navbar.Brand>
           </div>
           <div>
 
           <Nav className="me-auto">
-            <Nav.Link href={"/carrinho"}>Carrinho</Nav.Link>
-            <Nav.Link href={"/produto"}>Produtos</Nav.Link>
-            <Nav.Link href={"/sobrenos"}>Sobre Nós</Nav.Link>
-            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            <Nav.Link as={Link} to={"/carrinho"}>Carrinho</Nav.Link>
+            <Nav.Link as={Link} to={"/produto"}>Produtos</Nav.Link>
+            <Nav.Link as={Link} to={"/sobrenos"}>Sobre Nós</Nav.Link>
+            <Nav.Link onClick={handleLogout}>{loginOut}</Nav.Link>
           </Nav>
           
           </div>
