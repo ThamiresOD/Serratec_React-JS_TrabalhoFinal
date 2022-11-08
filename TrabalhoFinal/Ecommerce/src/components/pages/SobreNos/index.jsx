f13d44215ee147974e81f092a1e0b0655310e735
Andre-Toigo <andretoigo@hotmail.com>
2022-11-08 17:49:27 -0300

    Alterando nome

--- TrabalhoFinal/Ecommerce/src/components/pages/SobreNos/index.jsx
+++ TrabalhoFinal/Ecommerce/src/components/pages/SobreNos/index.jsx
@@ -31,7 +31,7 @@ function SobreNos() {
           </div>
           <div className="informacao">
             <img src={andre} alt="" />
-            <a href="https://github.com/Andre-Toigo">André Roigo</a>
+            <a href="https://github.com/Andre-Toigo">Andre Toigo</a>
           </div>
           <div className="informacao">
             <img src={thaisa} alt="" />
